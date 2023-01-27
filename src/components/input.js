c3bbc2a0054abc18bacddaaf948b4531c21cf6d9
Ushie01 <abugodwinaj@gmail.com>
2023-01-27 11:08:46 +0100

    Creation of Navbar

--- src/components/input.js
+++ src/components/input.js
@@ -5,14 +5,14 @@ import search from './../assets/search.svg';
 const input = () => {
     return (
       <div className="flex flex-row rounded-md items-center justify-center space-x-2 border-2 w-72">
-        <Link to="/" className="h-8 w-8 ml-2 mt-3">
-          <img src={search} alt={search} />
-        </Link>
-        <input
-          type="search"
-          className="h-12 w-48  text-xs border-white ml-1 font-black"
-          placeholder="Search Product"
-        />
+            <Link to="/" className="h-8 w-8 ml-2 mt-3">
+                <img src={search} alt={search} />
+            </Link>
+            <input
+                type="search"
+                className="h-12 w-48  text-xs border-white ml-1 font-black"
+                placeholder="Search Product"
+            />
       </div>
     );
 }
--- src/pages/Home/Homepage.js
+++ src/pages/Home/Homepage.js
@@ -1,12 +1,22 @@
 import Input from './../../components/input'; 
+import lightSun from './../../assets/dark-sun.svg';
+import notificationBell from './../../assets/notification-bell.svg'
 
     
 const Home = () => {
     return (
-        <nav className="w-98 h-20 border-2 border-black m-2">
-            <Input />
-        </nav>
-    )
+      <nav className="flex flex-row items-center justify-between w-98 p-3 h-20 bg-white">
+        <Input />
+        <div className='flex flex-row items-end justify-end space-x-4'>
+          <img src={lightSun} alt={lightSun} className="h-7 w-7" />
+          <img
+            src={notificationBell}
+            alt={notificationBell}
+            className="h-7 w-7 ml-2"
+          />
+        </div>
+      </nav>
+    );
 }
 
 export default Home;
