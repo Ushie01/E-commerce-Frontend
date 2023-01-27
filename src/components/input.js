3b9814bc22121efd657ad3ad700a5dc6c7225098
Ushie01 <abugodwinaj@gmail.com>
2023-01-27 10:19:26 +0100

    Creation of search input

--- src/components/input.js
+++ src/components/input.js
@@ -1,16 +1,20 @@
+import { Link } from 'react-router-dom';
+import search from './../assets/search.svg';
+
+
 const input = () => {
     return (
-        <div className="flex flex-row items-center justify-center bg-white 2xl:w-72">
-            <Link to="/" className="h-4 w-4 ml-2">
-                <img src={Search} alt={Search} />
-            </Link>
-            <input
-                type="search"
-                className="h-10 w-36 border-white ml-1"
-                placeholder="Search your task"
-            />
-        </div>
-    )
+      <div className="flex flex-row rounded-md items-center justify-center space-x-2 border-2 w-72">
+        <Link to="/" className="h-8 w-8 ml-2 mt-3">
+          <img src={search} alt={search} />
+        </Link>
+        <input
+          type="search"
+          className="h-12 w-48  text-xs border-white ml-1 font-black"
+          placeholder="Search Product"
+        />
+      </div>
+    );
 }
 
 export default input;
--- src/pages/Home/Homepage.js
+++ src/pages/Home/Homepage.js
@@ -1,7 +1,10 @@
+import Input from './../../components/input'; 
+
+    
 const Home = () => {
     return (
         <nav className="w-98 h-20 border-2 border-black m-2">
-            <input placeholder=""/>
+            <Input />
         </nav>
     )
 }
