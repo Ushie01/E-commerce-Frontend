999334156614d8c8a596c201ee87ecf1fc65109a
Ushie01 <abugodwinaj@gmail.com>
2023-02-09 02:11:05 +0100

    Building of SliderBar

---
+++ src/components/SliderBar/SliderBar.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+
+function valuetext(value) {
+  return `${value}°C`;
+}
+
+export default function RangeSlider() {
+  const [value, setValue] = React.useState([20, 37]);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+    return (
+        <>
+            <input value={value[0]} />
+            <input value={value[1]} />
+            <Box sx={{ width: 300 }}>
+                <Slider
+                    getAriaLabel={() => 'Temperature range'}
+                    value={value}
+                    onChange={handleChange}
+                    valueLabelDisplay="auto"
+                    getAriaValueText={valuetext}
+                />
+            </Box>
+        </>
+  );
+}
